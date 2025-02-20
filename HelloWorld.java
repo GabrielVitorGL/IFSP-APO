6b1a64a71b367054b7a4bd2049b0aa72df6164af
Gabriel-A89 <araujo.gabriel1@aluno.ifsp.edu.br>
2025-02-19 21:31:08 -0300

    aula 01 finalizada

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,12 @@
 
 public class HelloWorld {
-
+	public String msg;
+	
+	public HelloWorld() {	
+		this.msg = "Hello World!";
+	}
+	
+	public void show() {
+		System.out.println(this.msg);
+	}
 }
---
+++ Inicial.java
@@ -0,0 +1,10 @@
+
+public class Inicial {
+	
+	public static void main(String[] args) {
+		
+		HelloWorld helloWorld = new HelloWorld();
+		
+		helloWorld.show();
+	}
+}
